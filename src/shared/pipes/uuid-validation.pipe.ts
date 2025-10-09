86d0a8c3a40fd93d9ce4483c17c0c40962b34de4
Rody Huancas <rodyhuancas.04@gmail.com>
2025-10-08 22:54:43 -0500

    Feat: mejorar el manejo de errores en la validación de UUID

--- src/shared/pipes/uuid-validation.pipe.ts
+++ src/shared/pipes/uuid-validation.pipe.ts
@@ -4,7 +4,13 @@ import { BadRequestException, Injectable, ParseUUIDPipe } from '@nestjs/common';
 export class UUIDValidationPipe extends ParseUUIDPipe {
   constructor() {
     super({
-      exceptionFactory: () => new BadRequestException('El ID proporcionado no tiene un formato UUID válido'),
+      exceptionFactory: (error) =>
+        new BadRequestException({
+          statusCode: 400,
+          message   : 'El identificador proporcionado no es un UUID válido. Por favor, asegúrese de enviar un ID con el formato correcto.',
+          error     : 'UUID Validation Error',
+          details   : error || undefined,
+        }),
     });
   }
 }
